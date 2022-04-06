d41a276423648208982056c93a999b265d1f4233
mocysunshinework <mockysunshinework@gmail.com>
2022-04-06 04:31:50 +0000

    日時の設定を追加

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module AttendanceBApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zome = 'Asia/Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
