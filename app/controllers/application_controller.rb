2cf262d9a19fce521eff9a135f9d7cfbb14c1464
mocysunshinework <mockysunshinework@gmail.com>
2022-04-12 02:31:55 +0000

    ヘッダー編集中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # 全コントローラーの親クラスであるapplication_controller.rbにこのモジュールを
+  # 読み込ませることでどのコントローラーでもヘルバーに定義したメソッドが使えるようになる。
+  # 勤怠チュートリアル６章参照。
+  include SessionsHelper
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+  
   def new
   end
+  
+  def create
+    # ユーザーオブジェクトを取得
+    user = User.find_by(email: params[:session][:email].downcase) 
+    # ユーザーオブジェクトを取得でき、かつパスワードも一致した場合の判定はtrue
+    if user && user.authenticate(params[:session][:password])
+      # ログイン後にユーザー情報にリダイレクトする。
+      log_in user
+      redirect_to user
+    else
+      # ここにエラーメッセージ用のflashをいれる。
+      flash.now[:danger]= "ログインできませんでした。"
+    render :new
+    end
+  end
+  
 end
