09ff26bd972948bccd195464613db29cfe0d4214
mocysunshinework <mockysunshinework@gmail.com>
2022-04-14 02:24:56 +0000

    Finish editing-users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
   
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:show, :edit, :update, :index, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user,only: :destroy
+  
   def new
     @user = User.new(params[:id])
   end
   
   def show
-    @user = User.find(params[:id])
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
   end
   
   def create
@@ -19,10 +27,51 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "更新に成功しました。"
+      redirect_to user_url @user
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_path
+  end
+  
+
+  
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインして下さい"
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      redirect_to root_url unless current_user?(@user)
+    end
+    
+    def admin_user
+      unless current_user.admin?
+        redirect_to root_url
+      end
     end
 end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -43,6 +43,11 @@ module SessionsHelper
     # end
   # end
   
+  # 渡されたユーザーがログイン済みのユーザーであればtrueを返す。
+  def current_user?(user)
+    user == current_user
+  end
+  
   # 現在ログイン中のユーザーがいればtrue,そうでなければfaulseを返す。
   def logged_in?
     !current_user.nil?
