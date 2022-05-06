0b761d67b617dae073c8162e232f269446f257be
mocysunshinework <mockysunshinework@gmail.com>
2022-05-06 15:10:45 +0000

    for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20220415021520) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2022-05-02 08:00:00"
-    t.datetime "work_time", default: "2022-05-02 07:30:00"
+    t.datetime "basic_time", default: "2022-05-06 08:00:00"
+    t.datetime "work_time", default: "2022-05-06 07:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
