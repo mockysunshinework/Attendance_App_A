2ce4fc980d8e3468798d006b01b0d365aacd97ad
mockysunshinework <mockysunshinework@gmail.com>
2022-06-16 10:14:50 +0000

    カラムbasic_timeをbasic_work_timeに変更する前

--- app/models/user.rb
+++ app/models/user.rb
@@ -60,17 +60,16 @@ class User < ApplicationRecord
   end
   
   def self.import(file)
-    # 先頭行をヘッダとして読み取ってくれる
     CSV.foreach(file.path, headers: true) do |row|
       # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
       user = find_by(id: row["id"]) || new
       # CSVからデータを取得し、設定する
-      debugger
       user.attributes = row.to_hash.slice(*updatable_attributes)
       # 保存する
-      user.save
-    end
+      user.save!
+      end
   end
+  
 
   # 更新を許可するカラムを定義
   def self.updatable_attributes
