9cfa775ec8a7e89f8eb5666d1b22ac87837e26dd
mockysunshinework <mockysunshinework@gmail.com>
2022-06-28 14:46:36 +0000

    make resources 'bases' on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
       
     resources :attendances, only: :update
   end
+  
+  resources :bases
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
