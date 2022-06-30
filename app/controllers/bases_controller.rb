a56171785b3d91b7bb8da1f2b038ec59fe2ddabe
mockysunshinework <mockysunshinework@gmail.com>
2022-06-30 13:19:59 +0000

    拠点情報実装

--- app/controllers/bases_controller.rb
+++ app/controllers/bases_controller.rb
@@ -18,6 +18,30 @@ class BasesController < ApplicationController
     end
   end
   
+  def edit
+    @base = Base.find(params[:id])
+  end
+  
+  def update
+    @base = Base.find(params[:id])
+    if @base.update_attributes(base_params)
+      flash[:success] = "更新に成功しました。"
+      redirect_to bases_url
+    else
+      @bases = Base.all
+      render :index
+      flash[:danger] = "更新に失敗しました。"
+      redirect_to users_url @user
+    end
+  end
+  
+  def destroy
+    @base = Base.find(params[:id])
+    @base.destroy
+    flash[:success] = "拠点番号#{@base.base_number}、拠点名「#{@base.base_name}」を削除しました。"
+    redirect_to bases_path
+  end
+  
   private
     def base_params
       params.require(:base).permit(:base_number, :base_name, :attendance_type)
